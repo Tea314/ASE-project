2c400194f334b1f629bd031ee979f40619c5bfea
Huynh Lan Phuong <huynhlanphuong18082004@gmail.com>
2025-12-09 23:09:05 +0700

    remove temp file

--- scams-backend/test.py
+++
@@ -1,7 +0,0 @@
-import bcrypt
-
-passwords = ["alice123", "bob456", "charlie789"]
-
-for pw in passwords:
-    hashed = bcrypt.hashpw(pw.encode("utf-8"), bcrypt.gensalt())
-    print(hashed.decode("utf-8"))
